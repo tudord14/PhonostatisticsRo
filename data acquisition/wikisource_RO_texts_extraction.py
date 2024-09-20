703c85f765609f3ad4147889504d60503afb4357
tudor14 <tudordiac2002@gmail.com>
2024-09-20 23:29:35 +0200

    added functions

--- data acquisition/wikisource_RO_texts_extraction.py
+++ data acquisition/wikisource_RO_texts_extraction.py
@@ -4,11 +4,19 @@ import requests
 from bs4 import BeautifulSoup
 from ebooklib import epub
 import urllib.parse
+from bs4.element import Tag
+import re
 
 """
 
 STILL THINKING HOW TO DO IT!!!!!
 (wikisource is kinda strange to work with)
+
+UPDATE: 20.09 ->
+        -> the code obtains the texts from a specific romanian author
+        but the folder is not really "clean"
+        -> a follow up program for cleaning the folder will be created
+        ->(I hope it will happen soon)
 
 """
 
@@ -35,19 +43,56 @@ def search_texts(author_name):
 
                     if decoded_title == text_title:
                         link_to_book = f"https://ro.wikisource.org{html_a_elem['href']}"
-                        print(f"Title: {text_title}, Link: {link_to_book}")
                         text_titles.append(link_to_book)
-
-        print("Text titles found:")
-        for title in text_titles:
-            print(title)
-
-        return text_titles
-
+        return text_titles[1:]
     else:
         print(f"Error accessing page: {response.status_code}")
         return []
 
 
+def get_text_from_html(text_links):
+    texts = []
+    for link in text_links:
+        response = requests.get(link)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            text_title = link.replace("https://ro.wikisource.org/wiki/", "")
+
+            # The text is usually contained into the html div elemnt
+            # <div class="mw-content-ltr mw-parser-output" lang="ro" dir="ltr">
+            content_div = soup.find('div', class_='mw-parser-output')
+            if content_div:
+                for span in content_div.find_all('span'):
+                    span.decompose()
+
+                text_content = ""
+                for child in content_div.children:
+                    if isinstance(child, Tag) and child.name != 'span':
+                        text_content += child.get_text(separator="\n", strip=True) + "\n"
+
+                texts.append({'title': text_title, 'content': text_content})
+            else:
+                print(f"Content div not found {link}")
+        else:
+            print(f"Error when trying to access page: {response.status_code}")
+    return texts
+
+
+def transform_texts_to_txt(texts, author_name):
+    os.makedirs(author_name, exist_ok=True)
+
+    for text in texts:
+        title = urllib.parse.unquote(text['title'])
+        content = text['content']
+        sanitized_title = re.sub(r'[\\/*?:"<>|]', "_", title)
+        file_path = os.path.join(author_name, f"{sanitized_title}.txt")
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(content)
+
+        print(f"Saved '{title}' to '{file_path}'")
+
 name = input("Author name: ")
 all_text_titles = search_texts(name)
+texts = get_text_from_html(all_text_titles)
+transform_texts_to_txt(texts, name)
