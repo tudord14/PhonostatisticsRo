b85f6eef1a276ce8a1d8b18cc55de2d9f3438c13
tudor14 <tudordiac2002@gmail.com>
2024-09-23 23:09:38 +0200

    added functions

---
+++ data acquisition/store_data_Diac_method.py
@@ -0,0 +1,9 @@
+
+
+
+"""
+-> This will be (hopefully) my most ambitious implementation of my own idea!
+-> This came to me when I was thinking about storing large amounts of
+   txt files (22.09.2024)
+-> I think that in the next week I will write and try an implementation of it
+"""
