ceaf7f7fc07d6fce7db8e4bdce5ac4a9ff11556a
Tudor Diac <148883679+tudord14@users.noreply.github.com>
2024-09-30 22:13:44 +0200

    Update store_data_Diac_method.py

--- data acquisition/store_data_Diac_method.py
+++ data acquisition/store_data_Diac_method.py
@@ -2,23 +2,9 @@
 
 
 """
--> This will be (hopefully) my most ambitious implementation of my own idea!
--> This came to me when I was thinking about storing large amounts of
-   txt files (22.09.2024)
--> I think that in the next week I will write and try an implementation of it
 
-
-MAIN IDEA =>
-    -> create a big dictionary with a large amount of words and phrases that are commonly used in texts
-    -> map the words of the dictionary to numbers
-    -> each text will consist of "chain links" that together help build the text
-    -> this, in theory, should give us a list of pointers that point to the words from the list
-    -> really efficient!!!!!!!!
-    -> transform the texts into vectors/lists of numbers instead of words, thus saving storage
-    -> study the frequency of phrases and words repeated:
-            --> 3-word phrases
-            --> 4-word phrases
-            --> individual words
-    -> hopefully this helps at creating a local database that is not
+-> This will take some time...
+-> The idea came to me on 22.09.2024
+... working
 
 """
